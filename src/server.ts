8f3fb505667cd27712e717ded1a829861e39e4d7
Alka Pandit <alkapandit6101998@gmail.com>
2026-01-25 00:01:46 +0530

    feat: database connection pool created

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
-const express = require("express");
+import pool from "./common/config/db";
+import express from "express";
 
 const app = express();
 const PORT = 3000;
 
 // Server start
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("✅ Database Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Database connection failed", error);
+    process.exit(1);
+  }
+};
+
+startServer();
