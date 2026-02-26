990e0b13f5dbc228d15f8c7e166f8162877add9b
Alka Pandit <alkapandit6101998@gmail.com>
2026-02-26 08:36:27 +0530

    fix: async handler refactored

--- src/common/utils/asyncHandler.util.ts
+++ src/common/utils/asyncHandler.util.ts
@@ -1,13 +1,9 @@
 import { NextFunction, RequestHandler } from "express";
 
-// export const asyncHandler = (requestHandler: Function) => {
-//   return (req: Request, res: Response, next: NextFunction) => {
-//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
-
-export const asyncHandler =
-  (fn: RequestHandler): RequestHandler =>
-  (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+export const asyncHandler = (
+  requestHandler: RequestHandler,
+): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
+};
