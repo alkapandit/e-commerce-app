e9536ee59042628a775af3642cc1b4b9e92c2c9e
Alka Pandit <alkapandit6101998@gmail.com>
2026-03-09 21:21:35 +0530

    fix: user registration error handled in auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,14 +3,23 @@ import { sendResponse } from "../../common/utils/apiResponse.util";
 import { asyncHandler } from "../../common/utils/asyncHandler.util";
 import { HTTP_STATUS } from "../../constants/httpStatus.constant";
 import * as AuthService from "./auth.service";
+import { ApiError } from "../../common/utils/apiError.util";
 
 export const register = asyncHandler(async (req: Request, res: Response) => {
-  const user = await AuthService.register(req.body);
+  if (!req?.body) {
+    throw new ApiError(400, "Invalid request body!");
+  }
+
+  const user = await AuthService.register(req?.body);
+  if (!user) {
+    throw new ApiError(500, "Internal Server Error!");
+  }
+
   sendResponse({
     res,
     data: user,
     success: true,
-    statusCode: 201,
+    statusCode: HTTP_STATUS.CREATED,
     message: "User Registered Successfully!",
   });
 });
