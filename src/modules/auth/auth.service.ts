8c50db7c1a77a705efe0040ebd2db98fb28a15a1
Alka Pandit <alkapandit6101998@gmail.com>
2026-03-15 00:36:06 +0530

    add: login user service created with access token and refresh token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,13 @@
+import bcrypt from "bcryptjs";
+
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../../common/utils/token.util";
 import prisma from "../../common/config/prisma";
 import { ApiError } from "../../common/utils/apiError.util";
 import { HTTP_STATUS } from "../../constants/httpStatus.constant";
-import { BuyerRegisterInput } from "./auth.types";
-import bcrypt from "bcryptjs";
+import { BuyerLoginInput, BuyerRegisterInput } from "./auth.types";
 
 export const register = async (data: BuyerRegisterInput) => {
   const { email, firstName, lastName, password, phone } = data;
@@ -11,39 +16,97 @@ export const register = async (data: BuyerRegisterInput) => {
     throw new ApiError(HTTP_STATUS?.BAD_REQUEST, "All fields are mandatory!");
   }
 
-  const isUserExisting = await prisma.user.findFirst({
-    where: {
-      OR: [{ email }, { phone }].filter(Boolean),
-    },
-  });
+  try {
+    const isUserExisting = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { phone }].filter(Boolean),
+      },
+    });
 
-  if (isUserExisting) {
-    throw new ApiError(409, "User with email or phone number already exists!");
+    if (isUserExisting) {
+      throw new ApiError(
+        409,
+        "User with email or phone number already exists!",
+      );
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        firstName,
+        lastName,
+        phone,
+        passwordHash,
+        role: "BUYER",
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        email: true,
+        phone: true,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    console.error("Register Error:", error);
+
+    throw new ApiError(500, "Failed to register user");
   }
-
-  const passwordHash = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      firstName,
-      lastName,
-      phone,
-      passwordHash,
-      role: "BUYER",
-    },
-    select: {
-      id: true,
-      firstName: true,
-      email: true,
-      phone: true,
-    },
-  });
-
-  return user;
 };
 
-export const login = async (data: any) => {};
+export const login = async (data: BuyerLoginInput) => {
+  const { identifier, password } = data;
+
+  if (!identifier || !password) {
+    throw new ApiError(400, "All fields are required!");
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: identifier }, { phone: identifier }],
+      },
+    });
+
+    if (!user) {
+      throw new ApiError(404, "User does not exist!");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPasswordValid) {
+      throw new ApiError(401, "Invalid user credential!");
+    }
+
+    const accessToken = generateAccessToken(user?.id, user?.role);
+    const refreshToken = generateRefreshToken(user?.id, user?.role);
+
+    if (!accessToken || !refreshToken) {
+      throw new ApiError(500, "Error in generating refresh and access token!");
+    }
+
+    const hashRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    await prisma.$transaction([
+      prisma.user.update({
+        where: { id: user.id },
+        data: { refreshToken: hashRefreshToken },
+      }),
+    ]);
+
+    return {
+      user: { email: user?.email, role: user?.role },
+      accessToken,
+      refreshToken,
+    };
+  } catch (error: any) {
+    console.error("error in updating refresh token: ", error);
+    throw new ApiError(500, "Failed to login user!");
+  }
+};
 
 export const logout = async (userId: string) => {};
 
