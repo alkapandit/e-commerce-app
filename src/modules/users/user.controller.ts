136e92e8b4d6a231f4b76448df7fd16bb9e29e00
Alka Pandit <alkapandit6101998@gmail.com>
2026-03-25 00:07:07 +0530

    add: user controller and user services added

---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import * as UserService from "./user.service";
+import { HTTP_STATUS } from "../../constants/httpStatus.constant";
+import { sendResponse } from "../../common/utils/apiResponse.util";
+import { asyncHandler } from "../../common/utils/asyncHandler.util";
+
+export const getProfile = asyncHandler(async (req: Request, res: Response) => {
+  const user = await UserService.getUserById(req.body?.userId);
+  sendResponse({
+    res,
+    data: user,
+    success: true,
+    statusCode: HTTP_STATUS.OK,
+    message: "User fetched successfully",
+  });
+});
+
+export const updateProfile = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = await UserService.updateProfile(req.body?.userId, req.body);
+    sendResponse({
+      res,
+      data: user,
+      success: true,
+      statusCode: HTTP_STATUS.OK,
+      message: "Profile updated",
+    });
+  },
+);
+
+export const forgotPassword = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = await UserService.forgotPassword(req.body?.userId, req.body);
+    sendResponse({
+      res,
+      data: user,
+      success: true,
+      statusCode: HTTP_STATUS.OK,
+      message: "Password reset successfully",
+    });
+  },
+);
---
+++ src/modules/users/user.service.ts
@@ -0,0 +1,3 @@
+export const getUserById = async (id: string) => {};
+export const updateProfile = async (id: string, data: any) => {};
+export const forgotPassword = async (id: string, data: any) => {};
