840b7b19bd4c1a5aaa8397e28a6fefc7a51fe4b6
Alka Pandit <alkapandit6101998@gmail.com>
2026-04-29 22:26:06 +0530

    add: product routes added

---
+++ src/modules/products/product.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  validateBody,
+  validateParam,
+  validateQuery,
+} from "../../common/middlewares/validation.middleware";
+import * as ProductController from "./product.controller";
+
+const router = Router();
+
+router.post("/", validateBody, ProductController.getAllProducts);
+router.get("/search", validateQuery, ProductController.searchProduct);
+router.get("/:id", validateParam, ProductController.getProductById);
+router.put("/:id", validateParam, ProductController.updateProduct);
+
+export default router;
---
+++ src/modules/products/product.types.ts
@@ -0,0 +1,85 @@
+export type ProductSearchQueryInput = {
+  search?: string;
+  category?: string;
+  page?: string;
+  limit?: string;
+  minPrice?: string;
+  maxPrice?: string;
+};
+
+export type VariantOption = {
+  value: string;
+  price?: number;
+  stock?: number;
+};
+
+export type ProductVariant = {
+  name: string;
+  options: VariantOption[];
+};
+
+export type AddProductInput = {
+  name: string;
+  description?: string;
+
+  price: number;
+  discountPrice?: number;
+
+  categoryId: string;
+  subCategoryId?: string;
+
+  brand?: string;
+
+  sku?: string; // stock keeping unit
+  stock: number;
+
+  images: string[];
+
+  isActive?: boolean;
+  isFeatured?: boolean;
+
+  tags?: string[];
+
+  weight?: number;
+  dimensions?: {
+    length?: number;
+    width?: number;
+    height?: number;
+  };
+
+  variants?: ProductVariant[];
+
+  createdBy: string; // sellerId or adminId
+};
+
+export type UpdateProductInput = Partial<{
+  name: string;
+  description: string;
+
+  price: number;
+  discountPrice: number;
+
+  categoryId: string;
+  subCategoryId: string;
+
+  brand: string;
+
+  sku: string;
+  stock: number;
+
+  images: string[];
+
+  isActive: boolean;
+  isFeatured: boolean;
+
+  tags: string[];
+
+  weight: number;
+  dimensions: {
+    length: number;
+    width: number;
+    height: number;
+  };
+
+  variants: ProductVariant[];
+}>;
